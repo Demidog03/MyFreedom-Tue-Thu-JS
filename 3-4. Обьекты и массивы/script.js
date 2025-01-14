bc40c347fbbef20bddc45fa5b94d4e0a15faaaf7
Demidog03 <olzhasktl362@gmail.com>
2025-01-14 21:56:51 +0500

    5. Методы строк и массивов

--- 3-4. Обьекты и массивы/script.js
+++ 3-4. Обьекты и массивы/script.js
@@ -53,31 +53,31 @@
 // key: value -> ключ: значение
 // field: value -> поле: значение
 // parameter: value -> свойство: значение
-const phone1 = {
-  name: "iPhone 16",
-  storage: 256,
-  isNew: true,
-  color: "Розовый",
-  showInfo: function () {
-    showPhoneInfo(phone1);
-  }, // метод обьекта
-  getName: function () {
-    console.log("Название телефона: " + phone1.name);
-  },
-};
+// const phone1 = {
+//   name: "iPhone 16",
+//   storage: 256,
+//   isNew: true,
+//   color: "Розовый",
+//   showInfo: function () {
+//     showPhoneInfo(phone1);
+//   }, // метод обьекта
+//   getName: function () {
+//     console.log("Название телефона: " + phone1.name);
+//   },
+// };
 
-const phone2 = {
-  name: "Samsung Galaxy s24 ultra",
-  storage: 512,
-  isNew: false,
-  color: "Черный",
-  showInfo: function () {
-    showPhoneInfo(phone2);
-  },
-  getName: function () {
-    console.log("Название телефона: " + phone2.name);
-  },
-};
+// const phone2 = {
+//   name: "Samsung Galaxy s24 ultra",
+//   storage: 512,
+//   isNew: false,
+//   color: "Черный",
+//   showInfo: function () {
+//     showPhoneInfo(phone2);
+//   },
+//   getName: function () {
+//     console.log("Название телефона: " + phone2.name);
+//   },
+// };
 
 // console.log('Phone1:', phone1)
 // console.log('Phone2:', phone2)
@@ -142,5 +142,128 @@ const phone2 = {
 //   console.log(`Заработная плата ${key} составляет ${engineers[key]} рублей`);
 // }
 
+// const phone1 = {
+//   'model': 'Iphone 15',
+//   'price': 450000,
+//   'isSold': true,
+// }
 
+// phone1.color = 'red'
+// console.log(phone1)
+
+
+// for (const key in phone1) {
+//   console.log(phone1[key]) // phone1['isSold']
+// }
+
+// console.log(phone1['price'])
+
+
+// ARRAY (Массивы)
+// const phones = ['Iphone 15', 'Samsung s24', 'Google pixel', 'Oppo', 'Vivo', 'Honor', 'Xiaomi']
+// const prices = [450000, 230000, 170000, 120000]
+
+// console.log(phones[4])
+// console.log(phones.length)
+
+// FOR (array)
+// for(let i = 0; i < prices.length; i++) {
+//   console.log(prices[i]) // iphone[3]
+// }
+
+// for(let i = 0; i <= phones.length - 1; i++) {
+//   console.log(phones[i]) // iphone[3]
+// }
+
+
+// ЗАДАНИЕ 3
+// const numbers = [4, 35, 67, 8, 32, 76]
+// for(let i = 1; i < numbers.length; i+=2) {
+//   console.log(numbers[i]) // numbers[5]
+// }
+
+// ЗАДАНИЕ 4
+// const numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(`Индексу ${i} соответствует число ${numbers[i]}`)
+// }
+
+// for (const index in numbers) {
+//   console.log(`Индексу ${index} соответствует число ${numbers[index]}`)
+// }
+
+
+// FOR OF
+// const numbers = [4, 35, 67, 8, 32, 76]
+// for (const element of numbers) {
+//   console.log(element)
+// }
+// for (const index in numbers) {
+//   console.log(index)
+// }
+
+// const questions = [ 
+//   { 
+//     question: "What's the currency of the USA?", 
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"], 
+//     corAnswer: 0 
+//   },
+//   { 
+//     question: "Where was the American Declaration of Independence signed?", 
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"], 
+//     corAnswer: 0 
+//   },
+//   { 
+//     question: "Where was the American Declaration of Independence signed?", 
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"], 
+//     corAnswer: 0 
+//   },
+//   { 
+//     question: "Where was the American Declaration of Independence signed?", 
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"], 
+//     corAnswer: 0 
+//   }
+// ];
+
+// // const question = [#43242, #14343, #87686, #09678]
+
+// for (const question of questions) {
+//   question.userAnswer = null
+// }
+
+// questions[1].userAnswer = false
+
+// console.log(questions)
+
+
+
+// Методы PUSH, POP, UNSHIFT, SHIFT
+// const names = ['Olzhas', 'Suleymen', 'Kalizhan', 'Temirlan']
+// names.push('Dauren') // добавляет в конец массива новый элемент
+// names.push('Konstantin', 'Alexey')
+// names.pop() // удаляет последний элемент
+// names.pop()
+// names.pop()
+// console.log(names)
+
+// names.unshift('Tomiris')
+// names.unshift('Adiya', 'Nurgisa')
+// names.shift()
+// names.shift()
+// names.shift()
+// console.log(names)
+
+
+// ЗАДАНИЕ 7
+// const arr = [5, 4, 3, -3, -10, -1, 8, -20, 0]
+// const newArr = []
+
+// for (const element of arr) {
+//   if (element > 0) {
+//     newArr.push(element)
+//   }
+// }
+
+// console.log(newArr)
 
---
+++ 5. Методы строк и массивов/script.js
@@ -0,0 +1,33 @@
+// СТРЕЛОЧНЫЕ ФУНКЦИИ
+// const sayHello = () => {
+//     console.log('Hello!')
+// }
+
+// sayHello = 5
+
+// sayHello()
+
+// const sum = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// // const sum = (num1, num2) => num1 + num2
+
+// const result1 = sum(1, 1) // const result1 = 2
+
+// console.log(result1)
+
+
+// function adding (num1,num2) {
+//     if(arguments.length!=2){
+//         console.log("enter two numbers")
+//         return
+//     }
+//     if(typeof num1 !== "number" || typeof num2 !== "number"){
+//         console.log("You must enter numbers")
+//         return
+//     }
+//     console.log(num1+num2)
+// }
+// adding(7,'9')
+
